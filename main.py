b5fbd5ddb67b6ddd19c7b63857b313b25bb9cf8f
ana-kop <ana.kop20@gmail.com>
2020-12-14 17:04:05 +0000

    Importing data

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
+import rasterio.plot
+import pandas
+
+# Importing data
+
+# Elevation data
+path = "/Users/ak/Desktop/a_2/Material/"
+background = rasterio.open(path + "/background/raster-50k_2724246.tif")
+elevation = rasterio.open(path + "/elevation/SZ.asc")
+# count = 1 // CRS = BNG
+# rasterio.plot.show(elevation)
+
+# ITN data
+itn = pandas.read_json(path + "itn/solent_itn.json")
+print(itn.head())
+
+
+# Task 1
 # ask the user to input their current location as a British National Grid coordinate (easting and northing)
 
 print("--This program will help you find the the quickest route to walk to the highest point of land "
       "within a 5km radius.")
-#  test
+
 try:
     x = float(input("Please enter the Easting coordinate of your location: "))
 # If user entered not a number and there is ValueError, then:
@@ -22,15 +40,3 @@ inside = False
 
 if inside is False:
     print("Unable to assist in finding highest point of land.")
-
-import rasterio.plot
-path = "/Users/ak/Desktop/a_2/Material/"
-background = rasterio.open(path + "/background/raster-50k_2724246.tif")
-elevation = rasterio.open(path + "/elevation/SZ.asc")
-# count = 1 // CRS = BNG
-#rasterio.plot.show(elevation)
-
-#import ITN data
-import pandas
-itn = pandas.read_json(path + "itn/solent_itn.json")
-print(itn.head())
